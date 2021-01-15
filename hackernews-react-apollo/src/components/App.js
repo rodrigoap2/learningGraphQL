af9c5de55adadc66605e5231eb3c39fa54351f83
rodrigoap2 <rap2@cin.ufpe.br>
2021-01-15 10:11:15 -0300

    Create link creation mutation

--- hackernews-react-apollo/src/components/App.js
+++ hackernews-react-apollo/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './../logo.svg';
 import './../styles/App.css';
+import CreateLink from './CreateLink';
 import LinkList from './LinkList'
 
 function App() {
   return (
-    <LinkList/>
+    <CreateLink/>
   );
 }
 
